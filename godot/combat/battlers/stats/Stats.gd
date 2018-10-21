f525ed0fb8652c9d894fbd5bd714d535a0f04b47
Nathan Lovato <nathan@gdquest.com>
2018-10-22 08:42:50 +0900

    Fix #2 bar receiving damage taken instead of new health

--- godot/combat/battlers/stats/Stats.gd
+++ godot/combat/battlers/stats/Stats.gd
@@ -2,7 +2,7 @@ extends Node
 
 class_name CharacterStats
 
-signal health_changed(amount)
+signal health_changed(new_health)
 signal health_depleted()
 
 var modifiers = {}
@@ -21,7 +21,7 @@ func set_max_health(value):
 func take_damage(hit):
 	health -= hit.damage
 	health = max(0, health)
-	emit_signal("health_changed", hit.damage)
+	emit_signal("health_changed", health)
 	if health == 0:
 		emit_signal("health_depleted")
 
