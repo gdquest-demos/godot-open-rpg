ea00465cbea8c8fed71d50d55f0fbddf8908d040
Nicholas Hydock <nhydock@gmail.com>
2018-11-17 13:45:01 -0500

    Add Battle Formations

--- godot/Game.gd
+++ godot/Game.gd
@@ -4,6 +4,7 @@ var combat_arena = preload("res://combat/CombatArena.tscn").instance()
 
 onready var transition = $Overlays/TransitionColor
 onready var local_map = get_node("LocalMap")
+onready var party = $Party
 
 var transitioning = false
 func _ready():
@@ -22,7 +23,7 @@ func enter_battle(formation_name : String):
 		local_map.visible = false
 		add_child(combat_arena)
 		yield(get_tree().create_timer(0.8), "timeout")
-		combat_arena.initialize()
+	    combat_arena.initialize(formation, party)
 		yield(transition.fade_from_color(), "completed")
 		combat_arena.battle_start()
 		transitioning = false
