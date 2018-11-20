6b4fdfe94b4f27c0017eb0892acddcd7eec4c4e3
salvob <tholalak@yahoo.it>
2018-11-20 13:03:27 +0100

    cleanup incongruences between versions

--- godot/Game.gd
+++ godot/Game.gd
@@ -1,17 +1,24 @@
 extends Node
 
-onready var combat_arena = $CombatArena
+var combat_arena = preload("res://combat/CombatArena.tscn").instance()
+
 onready var transition = $Overlays/TransitionColor
+onready var local_map = get_node("local_map")
+
 
 func _ready():
-	enter_battle()
+	#enter_battle()
+	local_map.visible = true
 
-func enter_battle():
+
+func enter_battle(formation_name : String):
 	"""
 	Plays the combat transition animation and initializes the combat scene
 	"""
 	yield(transition.fade_to_color(), "completed")
-	combat_arena.initialize()
+	local_map.visible = false
+	add_child(combat_arena)
 	yield(get_tree().create_timer(0.8), "timeout")
+	combat_arena.initialize()
 	yield(transition.fade_from_color(), "completed")
 	combat_arena.battle_start()
