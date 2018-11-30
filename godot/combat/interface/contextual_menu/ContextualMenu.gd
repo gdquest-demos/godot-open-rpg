2f6c20343d518e8b7940af7be1780780b6b3cb8f
Nathan Lovato <nathan@gdquest.com>
2018-11-30 10:58:23 +0900

    Refactor ContextualMenu
    Add centered layout (centered around the Y axis)
    Add offset for the entire layout

--- godot/combat/interface/contextual_menu/ContextualMenu.gd
+++ godot/combat/interface/contextual_menu/ContextualMenu.gd
@@ -1,44 +1,52 @@
 extends Control
 
-const CONTEXTUAL_ACTION = preload("res://combat/interface/contextual_menu/ContextualAction.tscn")
+const ContextualAction = preload("ContextualAction.tscn")
 
-export var radius : int = 128 setget set_radius
-export(float, 0, 3.0) var action_spacing : float = 0.75 setget set_action_spacing
-export var circle_to_right : bool = true setget set_circle_to_right
+export(float, 0.0, 300.0) var radius : float = 190 setget set_radius
+export(float, 0, 1.0) var spacing : float = 0.2 setget set_spacing
+export(float, -1.0, 1.0) var offset : float = -0.1 setget set_offset
 
-var direction : int = 1
+enum Layout { CENTERED = 0, CLOCKWISE = 1, COUNTER_CLOCKWISE = -1}
+export(Layout) var layout : int = CENTERED
 
 func _ready() -> void:
-	#TODO: Remove this, just for testing
-	initialize([{'name': 'Attack'}, {'name': 'Run'}, {'name': 'Reload'}, {'name': 'Attack'}, {'name': 'Run'}, {'name': 'Reload'}])
+	initialize([{'name': 'Attack'}, {'name': 'Run'}, {'name': 'Reload'}, {'name': 'Attack'}, {'name': 'Run'}])
 
-func set_circle_to_right(new_value : bool) -> void:
-	circle_to_right = new_value
-	direction = 1 if circle_to_right else -1
-
-func set_radius(new_value : int) -> void:
+func set_radius(new_value : float) -> void:
 	radius = new_value
-	_recalculate_actions_positions()
+	_update_buttons_position()
 
-func set_action_spacing(new_value : float) -> void:
-	action_spacing = new_value
-	_recalculate_actions_positions()
+func set_spacing(new_value : float) -> void:
+	spacing = new_value
+	_update_buttons_position()
+
+func set_offset(new_value : float) -> void:
+	offset = new_value
+	_update_buttons_position()
 
 func initialize(actions : Array) -> void:
 	"""
 	Takes an array of actions to create the contextual menu entries
 	"""
-	for action_index in range(actions.size()):
-		var new_action = CONTEXTUAL_ACTION.instance()
-		var action_rotation = action_spacing * action_index * direction
-		new_action.rect_position = _get_action_position(action_index)
-		add_child(new_action)
-		new_action.initialize(actions[action_index], action_rotation)
+	for index in range(actions.size()):
+		var button = ContextualAction.instance()
+		add_child(button)
+		var action_rotation = spacing * index * layout
+		button.initialize(actions[index], action_rotation)
+	_update_buttons_position()
 
-func _get_action_position(index : int) -> Vector2:
-	return Vector2(0, -radius).rotated(action_spacing * index * direction)
-
-func _recalculate_actions_positions() -> void:
-	for action_index in range(get_child_count()):
-		var action = get_child(action_index)
-		action.rect_position = _get_action_position(action_index)
+func _update_buttons_position() -> void:
+	var button_count = get_child_count()
+	# The calculation is different if the menu is centered over the character,
+	# built clockwise, or counter-clockwise
+	var spacing_angle = spacing * PI
+	var start_offset_angle = offset * PI
+	if layout == CENTERED:
+		var centering_offset = spacing_angle / 2.0 * (button_count - 1)
+		for button in get_children():
+			var polar_angle = spacing_angle * button.get_index() - centering_offset
+			button.rect_position = Vector2(0, -radius).rotated(polar_angle + start_offset_angle)
+	else:
+		for button in get_children():
+			var polar_angle = spacing_angle * button.get_index() * layout
+			button.rect_position = Vector2(0, -radius).rotated(polar_angle + start_offset_angle)
