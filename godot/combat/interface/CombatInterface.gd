3cd0407e31542989c84e0b1be8b88bf1939b78f1
Nathan Lovato <nathan@gdquest.com>
2018-12-01 16:00:25 +0900

    Place circular menu based on the battler's extents

--- godot/combat/interface/CombatInterface.gd
+++ godot/combat/interface/CombatInterface.gd
@@ -9,6 +9,8 @@ onready var lifebar_builder = $BattlersBarsBuilder
 onready var select_arrow = $SelectArrow
 onready var popup = $PopUpHandler
 
+export var buttons_offset : Vector2 = Vector2(-75.0, 0.0)
+
 func initialize(battlers : Array):
 	lifebar_builder.initialize(battlers)
 	popup.initialize(battlers)
@@ -16,8 +18,8 @@ func initialize(battlers : Array):
 func open_actions_menu(battler : Battler) -> void:
 	var menu = CircularMenu.instance()
 	add_child(menu)
-	# TODO: Get height from the battler
-	menu.rect_position = battler.global_position - Vector2(70.0, 220.0)
+	var extents : RectExtents = battler.skin.get_extents()
+	menu.rect_position = battler.global_position - Vector2(0.0, extents.size.y) + buttons_offset
 	menu.initialize(battler)
 	var selected_action : CombatAction = yield(menu, "action_selected")
 	emit_signal("action_selected", selected_action)
