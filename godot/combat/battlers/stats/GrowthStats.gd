a7c55f8e7b6ede5bea3c0a3cc1330fd0b3f83018
Nathan Lovato <nathan@gdquest.com>
2018-12-04 13:01:43 +0900

    Replace experience array with a formula, closes #87

--- godot/combat/battlers/stats/GrowthStats.gd
+++ godot/combat/battlers/stats/GrowthStats.gd
@@ -2,31 +2,35 @@ extends CharacterStats
 
 class_name GrowthStats
 
-var MAX_LEVEL : int = 100 setget ,_get_max_level
+var MAX_LEVEL : int = 100
 var _interpolated_level : float
-export var experience_curve : Array
+
+export(float, 0.0, 10.0) var exp_multiplier : float = 1.0
+export(float, 0.1, 5.0) var exp_exponent : float = 1.8
+export(float, 0.0, 100.0) var exp_level_multiplier : float = 4.0
+
 export var max_health_curve : Curve
 export var max_mana_curve : Curve
 export var strength_curve : Curve
 export var defense_curve : Curve
 export var speed_curve : Curve
 
-func _get_max_level() -> int:
-	return int(max(1, len(experience_curve)))
-	
 func _set_experience(value : int = 0):
 	"""
 	Calculate level, which updates all stats
 	"""
 	experience = value
 	var l = level
-	while l + 1 < self.MAX_LEVEL && experience > experience_curve[l + 1]:
+	while l + 1 < self.MAX_LEVEL && experience > get_required_experience(l + 1):
 		l += 1
 	if l != level:
 		level = l
 		_interpolated_level =  float(level) / float(self.MAX_LEVEL)
 		reset()
-	
+
+func get_required_experience(level : int) -> int:
+	return int(round(exp_multiplier * pow(level, exp_exponent) + level * exp_level_multiplier))
+
 func _get_max_health() -> int:
 	return int(max_health_curve.interpolate_baked(_interpolated_level))
 
