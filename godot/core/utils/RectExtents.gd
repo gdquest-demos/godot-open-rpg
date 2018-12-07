2b6ad4923d58a32fe4830c4fcb5b3e1d90c4ac8e
Nathan Lovato <nathan@gdquest.com>
2018-12-07 19:54:01 +0900

    Add an add-on to edit RectExtents interactively
    Most of the work for #108
    
    Currently only resizes uniformly around the rectangle's center! It needs
    more code to add support for all 4 anchors. Or just fixing the issue
    below, restoring the move tool's functionality and deselect, could do
    the trick
    
    RectExtents's internals changed a little, now it calculates its own
    _rect property, a Rect2, based on its size and offset
    
    Known issues: The input code blocks the move tool and selecting other
    nodes at the moment
    
    This is an exercise to get a sense for Godot's EditorPlugin API and
    prepare some tutorial on the subject.
    
    Big thanks to xphere, I used his godot-zone plugin as a reference to get
    started :)

--- godot/core/utils/RectExtents.gd
+++ godot/core/utils/RectExtents.gd
@@ -7,21 +7,24 @@ extends Node2D
 
 class_name RectExtents
 
-export var size : Vector2 setget set_size
-export var offset : Vector2 setget set_offset
+export var size : Vector2 = Vector2(40.0, 40.0) setget set_size
 export var color : Color = Color("#ff0ea7") setget set_color
+export var offset : Vector2 setget set_offset
 
-var _base_offset : Vector2
-
-func set_size(value : Vector2) -> void:
-	size = value
-	_calculate_base_offset()
-	update()
+var _rect : Rect2
 
 func set_offset(value : Vector2) -> void:
 	offset = value
-	_calculate_base_offset()
+	_recalculate_rect()
 	update()
+
+func set_size(value : Vector2) -> void:
+	size = value
+	_recalculate_rect()
+	update()
+
+func _recalculate_rect():
+	_rect = Rect2(-1.0 * size / 2 + offset, size)
 
 func set_color(value : Color) -> void:
 	color = value
@@ -30,11 +33,8 @@ func set_color(value : Color) -> void:
 func _draw() -> void:
 	if not Engine.editor_hint:
 		return
-	draw_rect(Rect2(_base_offset + offset, size), color, false)
-
-func _calculate_base_offset():
-	_base_offset = -1.0 * Vector2(size.x / 2.0, size.y)
+	draw_rect(_rect, color, false)
 
 func has_point(point : Vector2) -> bool:
-	var as_rect = Rect2(global_position + _base_offset + offset, size)
-	return as_rect.has_point(point)
+	var rect_global = Rect2(global_position + _rect.position, _rect.size)
+	return rect_global.has_point(point)
