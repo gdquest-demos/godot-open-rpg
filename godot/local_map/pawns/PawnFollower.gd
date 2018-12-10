82e2c7185cab24862aa11a29e8b6d5126fdff838
Guilherme Henrique Teixeira de Oliveira <htoguilherme@gmail.com>
2018-12-10 17:18:30 -0200

    Visual destination point added to pawn leader when touch mode is active
    I opted to add it to the leader, as a sprite. 
    Closes #114

--- godot/local_map/pawns/PawnFollower.gd
+++ godot/local_map/pawns/PawnFollower.gd
@@ -2,6 +2,9 @@ extends PawnActor
 
 class_name PawnFollower
 
+func _ready() -> void:
+	destination_point.queue_free()
+
 func _on_target_Pawn_moved(last_position, current_position):
 	follow(last_position)
 
