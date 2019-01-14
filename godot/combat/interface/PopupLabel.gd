9bab97e094aba810892b37583732cccf84bb33e6
GodofGrunts <dergottdergrunten@gmail.com>
2019-01-14 15:09:50 -0500

    Added Popup for missed skills

--- godot/combat/interface/PopupLabel.gd
+++ godot/combat/interface/PopupLabel.gd
@@ -5,7 +5,7 @@ var _animation_name : String
 
 export var offset : Vector2 = Vector2(0.0, -40.0)
 
-func initialize(battler : Battler, type : String, difference : int):
+func initialize_damage(battler : Battler, type : String, difference : int):
 	"""
 	@type: either health or mana. Determines the animation the label will use
 	"""
@@ -15,5 +15,13 @@ func initialize(battler : Battler, type : String, difference : int):
 	get_node("Label").text = str(difference)
 	_animation_name = type + "_loss" if difference <= 0 else type + "_gain"
 
+func initialize_status(battler : Battler, type : String, message : String):
+	assert type in ["miss"]
+	var battler_extents : RectExtents = battler.skin.get_extents()
+	rect_global_position = battler.global_position - Vector2(0.0, battler_extents.size.y) + (offset * 2)
+	get_node("Label").text = message
+	_animation_name = type
+
 func play() -> void:
 	anim_player.play(_animation_name)
+	
--- godot/combat/interface/PopupLabelBuilder.gd
+++ godot/combat/interface/PopupLabelBuilder.gd
@@ -4,12 +4,20 @@ const label_scene = preload("PopupLabel.tscn")
 
 func initialize(battlers : Array) -> void:
 	for battler in battlers:
-		battler.stats.connect("health_changed", self, "spawn_label", [battler, "health"])
-		battler.stats.connect("mana_changed", self, "spawn_label", [battler, "mana"])
+		battler.stats.connect("health_changed", self, "spawn_label_number", [battler, "health"])
+		battler.stats.connect("mana_changed", self, "spawn_label_number", [battler, "mana"])
+		for skill in battler.skills.get_children():
+			skill.connect("attack_status", self, "spawn_label_text", [battler, "miss"])
 
-func spawn_label(new_value : int, old_value : int, battler : Battler, signal_type : String) -> void:
+func spawn_label_number(new_value : int, old_value : int, battler : Battler, signal_type : String) -> void:
 	var difference = new_value - old_value
 	var damage_label = label_scene.instance()
-	damage_label.initialize(battler, signal_type, difference)
+	damage_label.initialize_damage(battler, signal_type, difference)
+	add_child(damage_label)
+	damage_label.play()
+
+func spawn_label_text(signal_message : String, battler : Battler, signal_type : String) -> void:
+	var damage_label = label_scene.instance()
+	damage_label.initialize_status(battler, signal_type, signal_message)
 	add_child(damage_label)
 	damage_label.play()
