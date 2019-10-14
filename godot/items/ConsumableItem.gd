426a3b18ab050397c541145e49404923720a58b2
Nathan Lovato <nathan@gdquest.com>
2019-10-14 15:10:26 +0200

    Move all code to a src/ directory, pass 2

--- godot/items/ConsumableItem.gd
+++
@@ -1,8 +0,0 @@
-extends "res://items/Item.gd"
-class_name ConsumableItem
-
-# action to execute when used in combat
-export(Resource) var action = null setget ,_get_action
-	
-func _get_action() -> CombatAction:
-	return action as CombatAction
---
+++ godot/src/items/ConsumableItem.gd
@@ -0,0 +1,8 @@
+extends Item
+class_name ConsumableItem
+
+# action to execute when used in combat
+export(Resource) var action = null setget ,_get_action
+	
+func _get_action() -> CombatAction:
+	return action as CombatAction
