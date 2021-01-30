7c22d71d9578b8828303f3c1dfa4cb05e5319e53
PaulGGJ <paulsmart525@gmail.com>
2021-01-30 11:48:38 -0600

    fixed Cherry Slime

--- godot/src/combat/battlers/actions/Capture.gd
+++ godot/src/combat/battlers/actions/Capture.gd
@@ -11,8 +11,43 @@ func execute(targets):
 			var hit = Hit.new(1000)
 			#var combat_arena = target.get_parent().get_parent()
 			#combat_arena.capture_reward()
-			target.drops.get_children().push_back({'item': 'Slime.tres', 'amount': '1'})
+			
+			#target.drops.get_children().push_back({'item': 'Slime.tres', 'amount': '1'})
+			var monster_collection: MonsterCollection = get_node("/root/Game/MonsterCollection")
+			#This is not the most sensible place to be constructing the slime
+			#Its stats should instead get figured/copied from the target that was captured
+			var slime: Slime = Slime.new()
+			#slime.stats = load("res://src/slimes/CherrySlime.tres")
+			slime.stats = target.stats
+			slime.hp = slime.stats.max_health
+			slime.mp = slime.stats.max_mana
+			slime.xp = 0
+			monster_collection.add_slime(slime)
+			
 			target.take_damage(hit)
 		yield(actor.get_tree().create_timer(1.0), "timeout")
 		yield(return_to_start_position(), "completed")
 	return true
+	
+#func temp() -> void:
+#	var monster_collection: MonsterCollection = get_node("/root/Game/MonsterCollection")
+
+#	#This is not the most sensible place to be constructing the slime
+#	#Its stats should instead get figured from what was capture
+#	print("woo")
+#	var slime: Slime = Slime.new()
+#	slime.stats = load("res://src/slimes/CherrySlime.tres")
+#	slime.hp = slime.stats.max_health
+#	slime.xp = 0
+#	monster_collection.add_slime(slime)
+
+
+#func _ready() -> void:
+#	temp()
+
+
+
+
+
+
+
