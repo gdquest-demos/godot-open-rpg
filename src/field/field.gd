abc778a8467eacc73e0dab0887ce78b0cbce1436
Xananax <me@xananax.com>
2023-05-03 17:57:27 +0200

    consolidate initialization and global flow control

--- src/field/field.gd
+++ src/field/field.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@onready var camera: = $Camera2D as Camera2D
+
 ## The physics layers which will be used to search for gamepiece obejcts.
 ## Please see the project properties for the specific physics layers. [b]All[/b] collision shapes
 ## matching the mask will be checked regardless of position in the scene tree.
@@ -8,15 +10,60 @@ extends Node
 ## The physics layers which will be used to search for terrain obejcts.
 @export_flags_2d_physics var terrain_mask: = 0
 
-@onready var player: = $PlayerParty as FieldPlayer
+@export var focus: Gamepiece = null:
+	set = set_focus
+
+@export var is_active: = false:
+	set = set_is_active
 
 
-func initialize() -> void:
-	# Wait a single frame for the physics server to update.
-	await get_tree().process_frame
+func _ready() -> void:
+	randomize()
+	place_camera_at_focus()
+
+
+func place_camera_at_focus() -> void:
+	camera.reset_smoothing()
+
+
+func set_focus(value: Gamepiece) -> void:
+	if value == focus:
+		return
 	
-	player.initialize(gamepiece_mask, terrain_mask)
+	if focus:
+		focus.camera_anchor.remote_path = ""
 	
-	player.set_focus($Gameboard/Objects/Gamepieces/Player)
-	player.place_camera_at_focus()
-	player.is_active = true
+	focus = value
+	
+	if not is_inside_tree():
+		await ready
+	
+	# Free up any lingering player controllers.
+	for controller in get_tree().get_nodes_in_group(PlayerController.GROUP_NAME):
+		controller.queue_free()
+	
+	
+	if focus:
+		focus.camera_anchor.remote_path = focus.camera_anchor.get_path_to(camera)
+		
+		var new_controller = PlayerController.new()
+		new_controller.gamepiece_mask = gamepiece_mask
+		new_controller.terrain_mask = terrain_mask
+		
+		focus.add_child(new_controller)
+		new_controller.is_active = true
+
+
+func set_is_active(value: bool) -> void:
+	if not focus:
+		value = false
+	if value == is_active:
+		return
+	
+	is_active = value
+	
+	if not is_inside_tree():
+		await ready
+	
+	for controller in get_tree().get_nodes_in_group(PlayerController.GROUP_NAME):
+		(controller as PlayerController).is_active = is_active
--- src/main.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-
-func _ready() -> void:
-	randomize()
-	
-	$Field.initialize.call_deferred()
