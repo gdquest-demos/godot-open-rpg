6c100f3474ad53fe31c3fe718931dee7cbcbc990
food-please <food.please.dev@gmail.com>
2023-08-18 09:48:45 -0300

    Wipe clean for cleaner future commits

--- addons/dialogic/Modules/Background/node_background_holder.gd
+++
@@ -1,6 +0,0 @@
-class_name DialogicNode_BackgroundHolder
-extends CanvasLayer
-
-
-func _ready():
-	add_to_group('dialogic_background_holders')
--- src/common/globals/groups.gd
+++
@@ -1,8 +0,0 @@
-## All group names defined as constants.
-class_name Groups
-extends RefCounted
-
-const GAMEPIECES: = "_GP_GROUP"
-
-const CONTROLLERS: = "_CONTROLLER_GROUP"
-const PLAYER_CONTROLLERS: = "_PLAYER_CONTROLLER_GROUP"
