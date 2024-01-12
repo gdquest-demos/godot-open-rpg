a375bd25ba735ab32e33e5bcaed167a641dabc87
Caleb <food.please.dev@gmail.com>
2024-01-11 20:34:04 -0800

    Rework Adoring Fan timeline

--- maps/town/fan_interaction.gd
+++ maps/town/fan_interaction.gd
@@ -1,15 +1,9 @@
 extends InteractionTemplateConversation
 
-const TOKEN_CHECK_TIMELINE: = preload("res://maps/town/fan_of_four_tokens.dtl")
-const FINISHED_QUEST_TIMELINE: = preload("res://maps/town/fan_of_four_finished.dtl")
-
 @export var controller: GamepieceController
 
-var _quest_state: = 0
-
 @onready var _adoring_fan: = get_parent() as Gamepiece
-#@onready var _controller: = get_parent().get_node("Controller") as GamepieceController
-@onready var _popop: = $InteractionPopup as InteractionPopup
+@onready var _popup: = $InteractionPopup as InteractionPopup
 
 
 func _ready() -> void:
@@ -19,15 +13,13 @@ func _ready() -> void:
 
 
 func _execute() -> void:
-	match _quest_state:
-		1: timeline = TOKEN_CHECK_TIMELINE
-		2: timeline = FINISHED_QUEST_TIMELINE
-	
 	await super._execute()
 	
-	if _quest_state == 0:
+	# The quest's state is tracked by a Dialogic variable.
+	# After speaking with the character for the first time, he should run to a new position so that
+	# the player can speak with the other NPCs.
+	if Dialogic.VAR.get_variable("TokenQuestStatus") == 1:
 		await _on_initial_conversation_finished()
-		_quest_state = 1
 
 
 func _on_initial_conversation_finished() -> void:
@@ -35,11 +27,10 @@ func _on_initial_conversation_finished() -> void:
 	await _adoring_fan.arrived
 
 
-func _on_dialogic_signal_event(argument: String) -> void:
-	if argument == "receive_wand":
-		_quest_state = 2
-		_popop.is_active = false
-		
-		var inventory: = Inventory.restore()
-		inventory.remove(Inventory.ItemTypes.COIN, 4)
-		inventory.add(Inventory.ItemTypes.BLUE_WAND)
+# This conversation only emits a signal once: when the player should receive the quest reward.
+func _on_dialogic_signal_event(_argument: String) -> void:
+	_popup.is_active = false
+	
+	var inventory: = Inventory.restore()
+	inventory.remove(Inventory.ItemTypes.COIN, 4)
+	inventory.add(Inventory.ItemTypes.BLUE_WAND)
