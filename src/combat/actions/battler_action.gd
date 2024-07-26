942eba695330ee886418eab060afb022c570f61b
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-07-26 13:11:06 +0200

    minor changes 2

--- src/combat/actions/battler_action.gd
+++ src/combat/actions/battler_action.gd
@@ -7,22 +7,16 @@ class_name BattlerAction extends Resource
 
 ## An action-specific icon. Shown primarily in menus.
 @export var icon: Texture
-
 ## The 'name' of the action. Shown primarily in menus.
 @export var label: = "Base combat action"
-
 ## Tells the player exactly what an action does. Shown primarily in menus.
 @export var description: = "A combat action."
-
 ## Amount of energy required to perform the action.
 @export_range(0, 10) var energy_cost: = 0
-
 ## The action's [enum Elements.Types].
 @export var element: = Elements.Types.NONE
-
 @export var targets_self: = false
 @export var targets_all: = false
-
 ## The amount of [member Battler.readiness] left to the Battler after acting. This can be used to
 ## design weak attacks that allow the Battler to take fast turns.
 @export_range(0.0, 100.0) var readiness_saved: = 0.0
