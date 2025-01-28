f70073d63dc00122ee4f4a7f6e3efa109a1f7c74
Caleb <food.please.dev@gmail.com>
2025-01-28 16:02:46 -0400

    Dynamically validate action menu and keep menu focus

--- src/combat/ui/action_menu/ui_action_menu.gd
+++ src/combat/ui/action_menu/ui_action_menu.gd
@@ -21,36 +21,15 @@ signal action_selected(action: BattlerAction)
 				CombatEvents.player_battler_selected.emit(null)
 		)
 		
-		# Populate the menu with a list of actions.
-		for action in battler.actions:
-			var can_use_action: = battler.stats.energy >= action.energy_cost
-			
-			var new_entry = _create_entry() as UIActionButton
-			new_entry.action = action
-			new_entry.disabled = !can_use_action or is_disabled
-			
-			# Setup callbacks to the action entry's button signals. For these connections, wrap the
-			# lambda in parentheses so that we can bind arguments.
-			new_entry.pressed.connect(
-				# When an action is pressed, the menu should prevent being re-pressed by disabling
-				# the entire action menu. Also, forward along which action was pressed.
-				(func _on_action_button_pressed(battler_action: BattlerAction) -> void:
-					await close()
-					action_selected.emit(battler_action)
-					#CombatEvents.player_action_selected.emit(battler_action, battler)
-					).bind(action)
-			)
+		# If the battler's energy levels changed, re-evaluate which actions are available.
+		battler.stats.energy_changed.connect(
+			func _on_battler_energy_changed():
+				for entry: UIActionButton in _entries:
+					var can_use_action: = battler.stats.energy >= entry.action.energy_cost
+					entry.disabled = !can_use_action or is_disabled
+		)
 		
-			new_entry.focus_entered.connect(
-				# Move the cursor to show which entry is currently in focus.
-				(func _on_action_button_focus_entered(button: UIActionButton) -> void:
-					# Disabled buttons seem to still receive the focus_entered signal, hence the
-					# check.
-					if not button.disabled:
-						_menu_cursor.move_to(button.global_position + 
-							Vector2(0.0, button.size.y/2.0))
-					).bind(new_entry)
-			)
+		_build_action_menu()
 		
 		show()
 		fade_in()
@@ -59,21 +38,6 @@ signal action_selected(action: BattlerAction)
 func _ready() -> void:
 	hide()
 	set_process_unhandled_input(false)
-	
-	# When an action is chosen, this menu is merely hidden in case the player decides to go
-	# 'back' in the menu hierarchy and choose a new action.
-	#CombatEvents.player_targets_selected.connect(
-		#func _on_player_targets_selected(targets: Array[Battler]):
-			## The player did not choose targets. Unhide the menu so that the player can
-			## choose a new action.
-			#if targets.is_empty():
-				#fade_in()
-			#
-			## Alternatively, the player has selected targets for the chosen action, in which
-			## case the menu may be freed. It is no longer needed.
-			#else:
-				#queue_free()
-	#)
 
 
 # Capture any input events that will signal going "back" in the menu hierarchy.
@@ -93,3 +57,43 @@ func close() -> void:
 	set_process_unhandled_input(false)
 	await fade_out()
 	queue_free()
+
+
+# Populate the menu with a list of actions.
+func _build_action_menu() -> void:
+	assert(battler, "Must assign a Battler before building the action menu!")
+	
+	for action in battler.actions:
+		var can_use_action: = battler.stats.energy >= action.energy_cost
+		
+		var new_entry = _create_entry() as UIActionButton
+		new_entry.action = action
+		new_entry.disabled = !can_use_action or is_disabled
+		new_entry.focus_neighbor_right = "." # Don't allow input to jump to the player battler list.
+		
+		# Setup callbacks to the action entry's button signals. For these connections, wrap the
+		# lambda in parentheses so that we can bind arguments.
+		new_entry.pressed.connect(
+			# When an action is pressed, the menu should prevent being re-pressed by disabling
+			# the entire action menu. Also, forward along which action was pressed.
+			(func _on_action_button_pressed(battler_action: BattlerAction) -> void:
+				await close()
+				action_selected.emit(battler_action)
+				).bind(action)
+		)
+	
+		new_entry.focus_entered.connect(
+			# Move the cursor to show which entry is currently in focus.
+			(func _on_action_button_focus_entered(button: UIActionButton) -> void:
+				_menu_cursor.move_to(button.global_position + 
+					Vector2(0.0, button.size.y/2.0))
+				).bind(new_entry)
+		)
+	
+	# Link the focus order of the entries to loop through the menu easily.
+	var last_entry_index: = _entries.size()-1
+	if last_entry_index > 0:
+		var first_entry: = _entries[0]
+		var last_entry: = _entries[last_entry_index]
+		first_entry.focus_neighbor_top = first_entry.get_path_to(last_entry)
+		last_entry.focus_neighbor_bottom = last_entry.get_path_to(first_entry)
